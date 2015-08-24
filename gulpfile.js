aeadd2dbebb933ca5e508768f63b1b8e2c4c5141
Hao Chang <yngwie.chang@gmail.com>
2015-08-24 18:42:10 +1000

    Tried module gulp-rename.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
 
 gulp.task('scripts', function() {
     gulp.src('app/js/**/*.js')
+        .pipe(rename({
+            suffix: '.min'
+        }))
         .pipe(uglify())
-        .pipe(gulp.dest('app/jss'));
+        .pipe(gulp.dest('app/js'));
 });
 
 gulp.task('default', ['scripts']);
